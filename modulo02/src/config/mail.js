dd0af40d57b57809c742354517374cafa0736c97
rjardem <rjardem@vmware.com>
2019-11-04 15:28:27 -0300

    template email, there is a error to fix

---
+++ modulo02/src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: '2525',
+  secure: false,
+  auth: {
+    user: '75f1befd145947',
+    pass: '2c5097a94a25b3',
+  },
+  default: {
+    from: 'Equipe GoBarber',
+  },
+};
